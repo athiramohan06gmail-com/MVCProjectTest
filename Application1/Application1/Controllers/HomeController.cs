f9089d8285b56569046b6b51fe2baa28614641dd
athira mohan <athiramohan06@gmail.com>
2019-01-13 16:35:06 +0530

    commented viewbag

--- Application1/Application1/Controllers/HomeController.cs
+++ Application1/Application1/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Application1.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+           // ViewBag.Message = "Your application description page.";
 
             return View();
         }
